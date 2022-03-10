3f09e2eabdf377be69125c07ecd0ec7dacc6dff4
JoeyMallat <joeymallat.business@hotmail.com>
2022-03-10 16:23:02 +0100

    Updated db connection

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 
 include 'model.php';
 
-$db = connect_db("localhost", "krcannouncer", "user", "password");
+$db = connect_db("ec2-52-49-56-163.eu-west-1.compute.amazonaws.com",
+    "detdgbhsj8hsu1",
+    "ynmhcwmwnpgzjf",
+    "cfc93e1c78c5cc51b0ccc880c54bb48c23ed666783566428bb538b2f0a620067");
 
 /* Landing page */
 if (new_route('/krcannouncer/krc-omroeper/', 'get')) {
