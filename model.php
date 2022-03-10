d376e980088e987c9213db36159106963d7e6c4f
JoeyMallat <joeymallat.business@hotmail.com>
2022-03-10 16:25:21 +0100

    Fixed dsn

--- model.php
+++ model.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
 function connect_db($host, $db, $user, $pass){
     $charset = 'utf8mb4';
 
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $dsn = "postgres:host=$host;dbname=$db;charset=$charset";
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
